d121c3c14e490fa7903b9e451f24a20682e97865
keylen <kelenworks@gmail.com>
2025-03-07 11:15:25 +0800

    🐞 fix:rename

--- app/dynamicLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-
-const dynamicLayout = ({children}:{children:ReactNode}) => {
-  return <div>{children}</div>;
-};
-
-export default dynamicLayout;
--- config/index.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_URL =  process.env.NEXT_PUBLIC_API_URL || '没有设置API_URL'
-export const isDevelopment = process.env.NODE_ENV === 'development';
